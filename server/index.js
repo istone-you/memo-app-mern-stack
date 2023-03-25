24156d807835ebea5d60245f9fad7be827ce71ad
EC2 Default User <ec2-user@ip-172-31-18-11.ap-northeast-1.compute.internal>
2023-03-25 07:49:14 +0000

    バリデーション機能の追加

--- server/index.js
+++ server/index.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const CryptoJS = require("crypto-js");
-const User = require("./src/v1/models/user");
 const JWT = require("jsonwebtoken");
+const { body } = require("express-validator");
+const User = require("./src/v1/models/user");
 const app = express();
 const PORT = 5000;
 require("dotenv").config();
+
+app.use(express.json());
 
 // データベースに接続
 try {
@@ -20,27 +23,38 @@ catch (error) {
 }
 
 // ユーザー新規登録API
-app.post("/registar", async (req, res) => {
-    //パスワードの受け取り
-    const password  = req.body.password;
+app.post(
+    "/registar",
+    body("username")
+        .isLength({ min: 4 }
+        .withMessage("ユーザー名は4文字以上である必要があります")),
+    body("password")
+        .isLength({ min: 8 })
+        .withMessage("パスワードは8文字以上である必要があります"),
+    body("confirmPassword")
+        .isLength({ min: 8 })
+        .withMessage("パスワードは8文字以上である必要があります"),
+    async (req, res) => {
+        //パスワードの受け取り
+        const password = req.body.password;
 
-    try {
-        // パスワードのハッシュ化
-        req.body.password  = CryptoJS.AES.encrypt(password, process.env.SECRET_KEY);
-        // ユーザーの新規作成
-        const user = await User.create(req.body);
-        // JWTトークンの生成
-        const token = JWT.sign({ id: user._id }, process.env.TOKEN_SECRET_KEY, {
-            expiresIn: "1d",
-        });
-        // レスポンス
-        return res.status(200).json({ user, token });
-    }
-    catch (error) {
-        // エラーのレスポンス
-        return res.status(500).json(error);
-    }
-});
+        try {
+            // パスワードのハッシュ化
+            req.body.password = CryptoJS.AES.encrypt(password, process.env.SECRET_KEY);
+            // ユーザーの新規作成
+            const user = await User.create(req.body);
+            // JWTトークンの生成
+            const token = JWT.sign({ id: user._id }, process.env.TOKEN_SECRET_KEY, {
+                expiresIn: "1d",
+            });
+            // レスポンス
+            return res.status(200).json({ user, token });
+        }
+        catch (error) {
+            // エラーのレスポンス
+            return res.status(500).json(error);
+        }
+    });
 
 app.listen(PORT, () => {
     console.log("ローカルサーバー起動中・・・");
