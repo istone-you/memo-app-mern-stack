b5fc83341304b8870f0f3c6c6b543fd4d5d46967
EC2 Default User <ec2-user@ip-172-31-18-11.ap-northeast-1.compute.internal>
2023-03-25 07:27:12 +0000

    MongoDBへの接続を追加

--- server/index.js
+++ server/index.js
@@ -1,9 +1,45 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const CryptoJS = require("crypto-js");
+const User = require("./src/v1/models/user");
+const JWT = require("jsonwebtoken");
 const app = express();
 const PORT = 5000;
+require("dotenv").config();
 
-app.get("/", (req, res) => {
-    res.send("Hello World!");
+// データベースに接続
+try {
+    mongoose.connect(process.env.MONGODB_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
+    console.log("データベースに接続しました");
+}
+catch (error) {
+    console.log(error);
+}
+
+// ユーザー新規登録API
+app.post("/registar", async (req, res) => {
+    //パスワードの受け取り
+    const password  = req.body.password;
+
+    try {
+        // パスワードのハッシュ化
+        req.body.password  = CryptoJS.AES.encrypt(password, process.env.SECRET_KEY);
+        // ユーザーの新規作成
+        const user = await User.create(req.body);
+        // JWTトークンの生成
+        const token = JWT.sign({ id: user._id }, process.env.TOKEN_SECRET_KEY, {
+            expiresIn: "1d",
+        });
+        // レスポンス
+        return res.status(200).json({ user, token });
+    }
+    catch (error) {
+        // エラーのレスポンス
+        return res.status(500).json(error);
+    }
 });
 
 app.listen(PORT, () => {
