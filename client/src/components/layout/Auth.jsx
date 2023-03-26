a2cd911cc25e1a945c7e489bc43d5bb4061d4a84
istone <istoneyou1205@yahoo.co.jp>
2023-03-26 02:12:41 +0000

    アカウント登録画面の作成

--- client/src/components/layout/Auth.jsx
+++ client/src/components/layout/Auth.jsx
@@ -1,11 +1,24 @@
+import { Box, Container } from '@mui/system';
 import React from 'react'
 import { Outlet } from "react-router-dom";
+import logo from '../../assets/images/Notion_logo.png';
 
 const Auth = () => {
   return (
     <div>
-      Auth
-      <Outlet />
+      <Container component="main" maxWidth="xs">
+        <Box sx={{
+          marginTop: 4,
+          display: 'flex',
+          alignItems: 'center',
+          flexDirection: 'column',
+        }}
+        >
+          <img src={logo} alt="logo" style={{ width: 100, height: 100, marginBottom: 3 }} />
+          メモアプリ
+        </Box>
+        <Outlet />
+      </Container>
     </div>
   )
 }
--- client/src/components/pages/Register.jsx
+++ client/src/components/pages/Register.jsx
@@ -1,8 +1,50 @@
+import { Box, Button, TextField } from '@mui/material'
 import React from 'react'
+import { LoadingButton } from "@mui/lab";
+import { Link } from "react-router-dom";
 
 const Register = () => {
   return (
-    <div>Register</div>
+    <>
+      <Box component="form">
+        <TextField
+          fullWidth
+          id="username"
+          label="お名前"
+          margin="normal"
+          name="username"
+          required
+        />
+        <TextField
+          fullWidth
+          id="password"
+          label="パスワード"
+          margin="normal"
+          name="password"
+          type="password"
+          required
+        />
+        <TextField
+          fullWidth
+          id="confirmPassword"
+          label="確認用パスワード"
+          margin="normal"
+          name="confirmPassword"
+          type="password"
+          required
+        />
+        <LoadingButton
+          sx={{ mt: 3, mb: 2 }}
+          fullWidth
+          type="submit"
+          loading={false}
+          color="primary"
+          variant="contained"
+        >アカウント作成
+        </LoadingButton>
+      </Box>
+      <Button component={Link} to="/login">既にアカウントをお持ちの方はこちら</Button>
+    </>
   )
 }
 
